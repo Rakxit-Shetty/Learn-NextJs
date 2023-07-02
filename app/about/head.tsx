c22861be04d2e9345889a121f41cf31334c85a92
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-07-02 19:17:48 +0530

    loading

---
+++ app/about/head.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Head() {
+  return (
+    <>
+    <title>About Page</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <meta name="abt" content="abt page" />
+    <link rel="icon" href="/favicon.ico" />
+  </>
+  )
+}
